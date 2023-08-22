5fe896e89a8bab1eb5eca8da72ba4b35b8da0c77
Ideles <pyjy@yahoo.com>
2023-08-22 23:44:24 +0800

    add 4.0 artifacts and characters

--- src/store/test.ts
+++ src/store/test.ts
@@ -6,7 +6,7 @@ import { choice } from "@/ys/utils";
 //     Object.keys(CharacterData).filter((k) => !k.startsWith("Traveler"))
 // );
 
-const charKeys = ["Kirara", "Dehya", "Mika", "Kaveh", "Baizhu"];
+const charKeys = ["Lynette", "Lyney", "Freminet", "Wriothesley", "Neuvillette"];
 
 const testArts =
     process.env.NODE_ENV == "development"
